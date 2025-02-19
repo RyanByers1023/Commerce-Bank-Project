3c26ee461361ea6efe6a9378165308a73eb98c8e
Ryan Byers <ryanbyers1023@gmail.com>
2025-02-19 15:55:10 -0600

    Set up project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html", "./src/**/*.js"], // Scan all HTML and JS files in src],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
