2268f8670e0aa1142fbe2f8df923ffc94482294d
Ibrahim <ibrahimsammyayyad@gmail.com>
2025-04-18 12:19:54 -0500

    scuffed shit for DB

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     host: 'localhost',          // Your MySQL host
     user: 'ibra-ryz9ai',      // Your MySQL username (replace with your actual username)
     password: 'stock_password',  // Your MySQL password (replace with your actual password)
-    database: 'your_database_name',  // The database containing stock_test
+    database: 'stock_market',  // The database containing stock_test
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
